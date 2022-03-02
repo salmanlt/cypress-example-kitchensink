25eb218dea5bc25eed2b51192942ca651e4fdc84
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2022-03-02 10:39:04 +0000

    updated conf

--- cypress/integration/examples/location.spec.js
+++ cypress/integration/examples/location.spec.js
@@ -2,7 +2,7 @@
 
 context('Location', () => {
   beforeEach(() => {
-    cy.visit('http://example.cypress.io/commands/location')
+    cy.visit('https://example.cypress.io/commands/location')
   })
 
   it('cy.hash() - get the current URL hash', () => {
@@ -14,13 +14,13 @@ context('Location', () => {
     // https://on.cypress.io/location
     cy.location().should((location) => {
       expect(location.hash).to.be.empty
-      expect(location.href).to.eq('http://example.cypress.io/commands/location')
+      expect(location.href).to.eq('https://example.cypress.io/commands/location')
       expect(location.host).to.eq('localhost:8080')
       expect(location.hostname).to.eq('localhost')
-      expect(location.origin).to.eq('http://example.cypress.io/')
+      expect(location.origin).to.eq('https://example.cypress.io/')
       expect(location.pathname).to.eq('/commands/location')
       expect(location.port).to.eq('8080')
-      expect(location.protocol).to.eq('http:')
+      expect(location.protocol).to.eq('https:')
       expect(location.search).to.be.empty
     })
   })
